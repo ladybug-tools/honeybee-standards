dfee021e65330f82620dfa9c0c054ddf5e14e9cd
mostaphaRoudsari <sadeghipour@gmail.com>
2021-06-24 16:43:43 -0400

    feat: expose path to json files in `__init__.py`
    This change makes it easier to access the files from other libraries. I'm adding this specific case for using in honeybee-schema.

--- honeybee_standards/__init__.py
+++ honeybee_standards/__init__.py
@@ -1,1 +1,8 @@
 """honeybee-standards library."""
+import os
+
+__here__ = os.path.dirname(__file__)
+
+modifierset_lib = os.path.join(__here__, 'modifiersets', 'default.json')
+modifier_lib = os.path.join(__here__, 'modifiers', 'default.mat')
+radiance_default = os.path.join(__here__, 'radiance_default.json')
